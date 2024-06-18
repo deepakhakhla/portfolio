5c0ed3171caca470832746de9a6c8707b1c3e81f
Deepak Kumar <105661076+deepakhakhla@users.noreply.github.com>
2024-06-18 20:16:25 +0530

    Update me.js about contact form

--- me.js
+++ me.js
@@ -14,16 +14,3 @@ $(".close-bar").on("click", function(){
     $(".sidebar").removeClass("active");
 })
 
-function sendEmail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "deepakhakhladeveloper@gmail.com",
-        Password : "8416D60ABDC4956B2AD8D10BB4EC78FBF03F",
-        To : "deepakhakhla@gmail.com",
-        From : document.getElementById('email').value,
-        Subject : "New Contact from Enquiry",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
